c44860c947435a36bb21e937ab2837d973e3d6f8
Vaughn Hays <vhays@usc.edu>
2023-10-27 11:40:40 -0700

    Adding some completed problems -- most recently completed is the one called in main

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// mod two_sum;
+// mod product_of_array_except_self;
+// mod longest_consecutive_sequence;
+//mod valid_palindrome;
+// mod two_sum_sorted_input;
+// mod TODO_three_sum;
+mod valid_parentheses;
+
+fn main() {
+    let res = valid_parentheses::is_valid(String::from("(]"));
+    println!("{:?}", res)
+}
